caa5c6cfe182d42d1d69879351e9b8e44ff24e3a
jdhaisne <julien.dhaisne@gmail.com>
2021-11-11 20:10:56 +0100

    added button setup

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,11 @@
 <script setup lang="ts">
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
 import JdButton from './components/Jd-button.vue'
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
-  <JdButton>jdbg</JdButton>
+  <JdButton rounded>jdbg</JdButton>
   <JdButton disabled>jdbg</JdButton>
 </template>
 
--- src/components/Jd-button.vue
+++ src/components/Jd-button.vue
@@ -1,16 +1,44 @@
 <template>
-    <button :disabled="disabled">
-        <slot></slot>
+    <button :disabled="disabled" class ="jd-btn" :class="rootClasses">
+        <span v-if="$slots.default" class="jd-btn-text">
+            <slot></slot>
+        </span>
     </button>
 </template>
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { computed, defineComponent } from 'vue'
+import { Sizes } from "@/models/enums"
+import { Colors } from '../models/enums/Colors'
 
 export default defineComponent({
     props: {
-        disabled: { type: Boolean }
+        rounded:    { type: Boolean },
+        block:      { type: Boolean },
+        outlined:   { type: Boolean },
+        disabled:   { type: Boolean },
+        size:       {
+                    type: String,
+                    default: Sizes.NORMAL,
+                    validator: (value: Sizes) => Object.values(Sizes).includes(value),
+        },
+        colors:     { 
+                    type: String,
+                    default: Colors.NEUTRAL,
+                    validator: (value: Colors) => Object.values(Colors).includes(value),
+                    },
     },
-    setup() {
+
+    setup(props, { slots }) {
+        console.log(props)
+
+        const rootClasses = computed(() => [{
+            "jd-btn--empty":    !slots.default,
+            "jd-btn--outlined": props.outlined,
+            "jd-btn--block":    props.block,
+            "jd-btn--rounded":  props.rounded
+        }])
+        
+        return { rootClasses }
         
 
     },
