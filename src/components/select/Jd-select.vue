d95052867ac2d72c7b65dd857a376d383a086c93
jdhaisne <julien.dhaisne@gmail.com>
2022-02-10 12:28:32 +0100

    added select

---
+++ src/components/select/Jd-select.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="jd-select">
+        <span class="jd-textfield-label" v-if="labelTop && !$slots.labelTop">{{ labelTop }}</span>
+        <span class="jd-textfield-label" v-if="$slots.labelTop">{{ $slots.labelTop }}</span>
+        <div class="jd-select-inner">
+            <div class="jd-select-selection">
+
+            </div>
+        </div>
+        <transition name="drop-bottom">
+            <div class="jd-select-dropdown">
+                <ul>
+                    <li>
+
+                    </li>
+                </ul>
+            </div>
+        </transition>
+    </div>
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    name: 'jd-select',
+    inheritAttrs: false,
+    props: {
+        labelTop:   {type: String},
+        options:    {type: Array, default: () => []}
+    },
+    setup() {
+        
+    },
+})
+</script>
